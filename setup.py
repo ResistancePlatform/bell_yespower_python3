75313b17c176db9cdf75f8dde7b4867e6faaa278
Luke Wegryn <luke@resistance.io>
2019-08-15 11:53:16 -0600

    Added in Relevant Resistance Yespower Files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 
-bell_yespower_module = Extension('bell_yespower',
+yespower_module = Extension('resistance_yespower',
                             sources = ['yespower-module.c',
                                        'yespower.c',
                                        'yespower-opt.c',
@@ -9,10 +9,10 @@ bell_yespower_module = Extension('bell_yespower',
                             extra_compile_args=['-O2', '-funroll-loops', '-fomit-frame-pointer'],
                             include_dirs=['.'])
 
-setup (name = 'bell_yespower',
-       version = '1.0.3',
-       author_email = 'yuto_tetuota@yahoo.co.jp',
-       author = 'y-chan',
-       url = 'https://github.com/bellcoin-electrum/bell_yespower_python3',
-       description = 'Bindings for yespower-1.0 proof of work used by bellcoin',
-       ext_modules = [bell_yespower_module])
+setup (name = 'resistance_yespower',
+       version = '1.0.1',
+       author_email = 'developer@resistance.io',
+       author = 'Resistance Developers',
+       url = 'https://github.com/ResistancePlatform/resistance_yespower_python3',
+       description = 'Bindings for yespower-1.0.1 proof of work used by Resistance',
+       ext_modules = [resistance_yespower_module])
